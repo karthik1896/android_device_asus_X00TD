84d161bdddf6d4520f95f0aa767c2c9e29404a04
karthik1896 <karthik1896@outlook.com>
2023-12-16 11:53:26 +0530

    Revert "X00TD: Import independent SmartCharging"
    This reverts commit 7e6d89c8b002d2147199a3fcba0a96e5bef874cf.

--- device.mk
+++ device.mk
@@ -429,10 +429,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
-# Smart Charging
-PRODUCT_PACKAGES += \
-    SmartCharging
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
