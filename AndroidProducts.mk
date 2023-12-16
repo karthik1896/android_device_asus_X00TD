fcc3fb56f41e6ce0b7192dcf000f030e44df3c4a
karthik1896 <karthik1896@outlook.com>
2023-12-16 11:46:53 +0530

    X00TD: Adapt to VoltageOS
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00TD.mk
+    $(LOCAL_DIR)/voltage_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X00TD-userdebug
+    voltage_X00TD-userdebug \
+    voltage_X00TD-user \
+    voltage_X00TD-eng
 
--- voltage_X00TD.mk
+++ voltage_X00TD.mk
@@ -9,17 +9,28 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common VoltageOS stuff
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 2160
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := voltage_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_X00TD
 PRODUCT_MANUFACTURER := asus
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+TARGET_VENDOR := asus
+TARGET_VENDOR_PRODUCT_NAME := X00TD
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=X00TD \
+    PRODUCT_DEVICE=X00TD \
+    PRODUCT_NAME=WW_X00TD \
+    PRIVATE_BUILD_DESC="sdm660_64-user 8.1.0 OPM1 1309 release-keys"
